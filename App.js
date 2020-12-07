b991ce9d30f061a0632db9a5a1a2b7bdf13d41cb
winbigcoms <bigcoms6290@gmail.com>
2020-12-07 16:50:31 +0900

    Feat app

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [viewText,setText] = useState("");
+  let i=0;
+  const text = "안녕하세요 프론트 엔드 개발자 입니다.";
+  const gogo = ()=>{
+    if(i===text.length){
+      clearInterval(gogo)
+    }else{
+      setText(vT=>vT+text[i]);
+      i++
+    }
+  }
+  useEffect(() => {
+    setInterval(gogo,500);    
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Button title="gogo" onPress={()=>{
+          setText(text=>text+"하")
+        }}/>
+        <Text style={textStyle.text}>
+          {viewText}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +39,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  innerContainer:{
+    flex:1,
+    backgroundColor:"pink",
+  }
 });
+
+const textStyle = StyleSheet.create({
+  text:{
+    color:"red",
+    fontSize:15
+  }
+})
